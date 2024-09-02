a105eb9808aa9a3614767bc58af732b78b02421e
Sharif <mdsh8692@gmail.com>
2024-09-02 21:20:09 +0600

    Finally Challenge section done and responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,7 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'bg-form': "url('/assets/bg.jpg')",
-
-      },
+     
     },
   },
   plugins: [],
